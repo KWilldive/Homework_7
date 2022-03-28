8ccc43918522f08e01cfc3a3d54650103dc4fa0d
KWilldive <102100616+KWilldive@users.noreply.github.com>
2022-03-28 12:31:16 -0500

    Add files via upload

--- ..Rankine_app.py
+++ ..Rankine_app.py
@@ -1,56 +1,66 @@
 import sys
-from PyQt5.QtWidgets import QDialog, QApplication
-from PyQt5.QtWidgets import QFileDialog,QMessageBox
-from PyQt5.QtGui import QCursor
+from PyQt5.QtWidgets import QWidget, QApplication
+from Rankine_GUI import Ui_Form  # from the GUI file your created
+from Calc_state import Steam_SI as steam
+from Rankine import rankine
+from Steam import steam
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-from Rankine_GUI import Ui_Form
-from Rankine import rankine  #importing classes
 
-class main_window(QDialog):
+class main_window(QWidget, Ui_Form):
     def __init__(self):
-        super(main_window,self).__init__()
+        super().__init__()
+        self.setupUi(self)
+        self.setWindowTitle('Rankine Cycle Calculator')
 
-        self.useX = True
+        self.rankine = rankine()
+        self.label = [self.le_PHigh, self.le_PLow, self.le_TurbineInletCondition, self.le_TurbineEff, self.le_H1,
+                      self.le_H2, self.le_H3, self.le_H4, self.le_HeatAdded, self.le_PumpWork, self.le_Efficiency,
+                      self.le_TurbineWork]
 
-
-        self.ui = Ui_Form()
-        self.ui.setupUi(self)
-        self.assign_widgets()
+        self.btn_Calculate.clicked.connect(self.Calculate)
         self.show()
-
-    def assign_widgets(self):
-        #self.ui.ExitButton.clicked.connect(self.ExitApp)
-        self.ui.btn_Calculate.clicked.connect(self.Calculate)
-
 
 
     def Calculate(self):
-        p_low = float(self.ui.le_PLow.text())
-        p_high = float(self.ui.le_PHigh.text())
-        t_high = float(self.ui.le_TurbineInletCondition.text())
-        t_eff = float(self.ui.le_TurbineEff.text())
 
-        if self.useX is True:
-            r = rankine(p_low, p_high)
-        else:
-            r = rankine(p_low, p_high, t_high=t_high)
+        self.rankine.p_high = float(self.le_PHigh.text()) * 100
+        self.rankine.p_low = float(self.le_PLow.text()) * 100
+        self.rankine.eff_turbine = float(self.le_TurbineEff.text())
 
-        eff = r.calc_efficiency()
+        self.rankine.quality = float(self.le_TurbineInletCondition.text()) if self.rdo_Quality.isChecked() else None
+        self.rankine.t_high = float(self.le_TurbineInletCondition.text()) if self.rdo_THigh.isChecked() else None
 
-        self.ui.le_TurbineInletCondition.setText('{:.1f}'.format(r.turbine_work))
-        self.ui.le_PumpWork.setText('{:.1f}'.format(r.pump_work))
-        self.ui.le_HeatAdded.setText('{:.1f}'.format(r.heat_added))
-        self.ui.le_H1.setText('{:.1f}'.format(r.state1))
-        self.ui.le_H2.setText('{:.1f}'.format(r.state2))
-        self.ui.le_H3.setText('{:.1f}'.format(r.state3))
-        self.ui.le_H4.setText('{:.1f}'.format(r.state4))
-        self.ui.le_Efficiency.setText('{:.3f}'.format(eff))
+        if self.rdo_Quality.isChecked():
+            self.lbl_TurbineInletCondition.setText(QtCore.QCoreApplication.translate("Form", "Turbine Inlet: x ="))
+        if self.rdo_THigh.isChecked():
+            self.lbl_TurbineInletCondition.setText(QtCore.QCoreApplication.translate("Form", "Turbine Inlet: T_high ="))
+
+        self.rankine.calc_efficiency()
+
+        self.le_H1.setText("{:.2f}".format(self.rankine.state1.h))
+        self.le_H2.setText("{:.2f}".format(self.rankine.state2.h))
+        self.le_H3.setText("{:.2f}".format(self.rankine.state3.h))
+        self.le_H4.setText("{:.2f}".format(self.rankine.state4.h))
+        self.le_HeatAdded.setText("{:.2f}".format(self.rankine.heat_added))
+        self.le_PumpWork.setText("{:.2f}".format(self.rankine.pump_work))
+        self.le_Efficiency.setText("{:.2f}".format(self.rankine.efficiency))
+        self.le_TurbineWork.setText("{:.2f}".format(self.rankine.turbine_work))
+
+        self.lbl_SatPropHigh.setText(
+            "\nPSat = {:.2f} bar, TSat= {:.2f} C\nhf = {:.2f} kJ/kg , hg = {:.2f} kJ/kg\nsf= {:.2f} kJ/kg*K, sg= {:.2f} kJ/kg*K\nvf= {:.4f} m^3/kg, vg= {:.2f} m^3/kg".format(
+                self.rankine.p_high / 100, self.rankine.state1.T, self.rankine.state1.hf, self.rankine.state1.hg,
+                self.rankine.state1.sf, self.rankine.state1.sg, self.rankine.state1.vf, self.rankine.state1.vg))
+        self.lbl_SatPropLow.setText(
+            "\nPSat = {:.2f} bar, TSat= {:.2f} C\nhf = {:.2f} kJ/kg , hg = {:.2f} kJ/kg\nsf= {:.2f} kJ/kg*K, sg= {:.2f} kJ/kg*K\nvf= {:.4f} m^3/kg, vg= {:.2f} m^3/kg".format(
+                self.rankine.p_low / 100, self.rankine.state2.T, self.rankine.state2.hf, self.rankine.state2.hg,
+                self.rankine.state2.sf, self.rankine.state2.sg, self.rankine.state2.vf, self.rankine.state2.vg))
 
         return
 
-
     def ExitApp(self):
         app.exit()
+
 
 if __name__ == "__main__":
     app = QApplication.instance()
